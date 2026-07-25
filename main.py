382c86560107d2059045ba49cf3f50c0231dadf3
Fabian Pfriem <contact@fabianpfriem.com>
2026-07-25 14:56:10 +0200

    removed template code

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ from database import store_sensor_data
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.post("/environment/sensors")
 def write_sensor_data(sensor_data: SensorData):
     row_id = store_sensor_data(sensor_data.model_dump())
     return {"message": "Sensor data received", "data": sensor_data, "stored_id": row_id}
-
 
 @app.get("/environment")
 def predict_environment(lux: float, sun_elevation: float, sun_azimut: float, season: str):
