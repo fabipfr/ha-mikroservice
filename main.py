8cd22c13154c8714ad6e34a3363458b7f6cf2751
Fabian Pfriem <contact@fabianpfriem.com>
2026-07-25 14:53:53 +0200

    added get environment prediction endpoint [WIP]

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ def read_root():
 def write_sensor_data(sensor_data: SensorData):
     row_id = store_sensor_data(sensor_data.model_dump())
     return {"message": "Sensor data received", "data": sensor_data, "stored_id": row_id}
+
+
+@app.get("/environment")
+def predict_environment(lux: float, sun_elevation: float, sun_azimut: float, season: str):
+    # Here we will let the model predict the environment based on the submitted features.
+    return {"prediction": "Day", "certainty": 0.95}
